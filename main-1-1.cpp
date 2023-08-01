39753cc4867abddabc26a67e173593eb908c8139
randolfcastillo <randolf4castillo@gmail.com>
2023-08-01 14:55:46 +0930

    first commit

---
+++ main-1-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+extern int sum_diagonal(int[4][4]);
+
+int main(){
+    int matrix[4][4]={{1, 2, 3, 4}, {5, 6, 7, 8 }, {9, 10, 11, 12}, {13, 14, 15,16}};
+
+    std :: cout << "The sum of the main diagonal is: " << sum_diagonal(matrix) << std :: endl;
+
+    return 0;
+}
