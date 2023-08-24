d9606846ca003fd0c443e92ba46dcbb756639864
randolfcastillo <randolf4castillo@gmail.com>
2023-08-24 15:18:44 +0930

    update

---
+++ StockItem.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+#include <string>
+#include "StockItem.h"
+
+StockItem (){ 
+barcode2=0;
+item_name="";
+}
+
+StockItem(int code_num, std::string description){
+    barcode=
+StockItem();          
+StockItem(int code_num, std::string description);      
+std::string get_description(){return barcode};    // returns the item description
+int get_item_code(){return item_name}      // returns the item code of the item
+~StockItem();  
+
+}
---
+++ StockItem.h
@@ -0,0 +1,16 @@
+#ifndef STOCKITEM_H
+#define STOCKITEM_H
+#include <string>
+class StockItem{
+private: 
+int barcode; 
+std :: string item_name;
+public:
+int barcode2; 
+StockItem();                  
+StockItem(int code_num, std::string description);      
+std::string get_description();    // returns the item description
+int get_item_code();       // returns the item code of the item
+~StockItem();               //A default destructor
+};
+#endif
