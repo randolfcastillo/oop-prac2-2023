3a63b634418e1060631dc833d68c076de8284866
randolfcastillo <randolf4castillo@gmail.com>
2023-09-07 14:33:41 +0930

    test

---
+++ Tesla.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "Car.h"
+#include "Tesla.h"
+
+Tesla :: Tesla(){
+
+}
+
+Tesla :: Tesla(char model, int price){
+    this->model=model;
+    this->price=price;
+}
+
+char Tesla :: get_model(){
+    return model;
+}
+void Tesla :: set_model(char model){
+    this->model=model;
+}
+
+float Tesla:: get_batteryPercentage(){
+    return batteryPercentage;
+}
+
+void Tesla :: set_batteryPercentage(float batteryPercentage){
+    if (batteryPercentage>100){
+        batteryPercentage=100;
+    }
+    else if(batteryPercentage<0){
+        batteryPercentage=0;
+    }
+    else{
+        this->batteryPercentage=batteryPercentage;
+    }
+}
+
+void Tesla :: chargeBattery(int mins){
+    while ((batteryPercentage<100) && (batteryPercentage>0))
+    {
+        batteryPercentage=batteryPercentage+(mins*0.5);
+        if (batteryPercentage>100){
+            break;
+        }
+    }
+     
+    
+}
+void Tesla :: drive(int kms){
+while(batteryPercentage<100 && batteryPercentage<0){
+    emissions=74*kms;
+    batteryPercentage=batteryPercentage-(0.01*5*kms);
+}
+
+}
---
+++ main-2-1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Car.h"
+#include "Tesla.h"
+int main(){
+Tesla Model_X('x',10000);
+  std :: cout << Model_X.get_price()<<std :: endl;
+}
+
