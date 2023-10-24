495476b0803ae2fc511e9a7446c53e436e62d004
randolfcastillo <randolf4castillo@gmail.com>
2023-10-24 17:11:20 +1030

    m

---
+++ Game.h
@@ -0,0 +1,50 @@
+#ifndef GAME_H
+#define GAME_H
+#include <iostream>
+#include "GameEntity.h"
+#include "Effect.h"
+#include <cstdlib>
+#include "Utils.h"
+#include "Ship.h"
+#include "Mine.h"
+
+class Game{
+    private:
+    std::vector<GameEntity*> entities;
+    public:
+    //Getter
+ std::vector<GameEntity*>& getEntities() {
+        return entities;
+    }
+
+    // Setter for entities
+    void setEntities(const std::vector<GameEntity*>& entities) {
+        this->entities=entities;
+    }
+std::vector<GameEntity*> initGame(int numShips, int numMines, int gridWidth, int gridHeight){
+     std::vector<GameEntity*> newEntities;
+
+        // Create Ship objects with random positions
+       for (int i = 0; i < numShips; ++i) {
+        std::tuple<int, int> randomPos = Utils::generateRandomPos(gridWidth, gridHeight);
+        Ship* ship = new Ship(std::get<0>(randomPos), std::get<1>(randomPos));
+        newEntities.push_back(ship);
+    }
+
+    for (int i = 0; i < numMines; ++i) {
+        std::tuple<int, int> randomPos = Utils::generateRandomPos(gridWidth, gridHeight);
+        Mine* mine = new Mine(std::get<0>(randomPos), std::get<1>(randomPos));
+        newEntities.push_back(mine);
+    }
+
+    entities = newEntities;
+    return newEntities;
+    
+    }
+void gameLoop(int maxIterations, double mineDistanceThreshold){
+    
+}
+
+};
+
+#endif
