c3eaf6c1b8a94363e68f046dfaa7fc367c971e93
randolfcastillo <randolf4castillo@gmail.com>
2023-10-24 16:28:44 +1030

    m

--- Mine.h
+++ Mine.h
@@ -4,7 +4,18 @@
 #include "Effect.h"
 #include "Explosion.h"
 class Mine : public GameEntity{
+private:
+public:
+Mine(int x, int y){
+    position=std::make_tuple(x,y);
+    type='M';
+}
 
+Explosion explode(){
+    type='X';
+    Explosion explosion(std::get<0>(position), std::get<1>(position));
+    return explosion;
+}
 };
 
 #endif
