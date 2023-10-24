e2fc9f66172b3b5bc5c362601eccfc73ce6989ec
randolfcastillo <randolf4castillo@gmail.com>
2023-10-24 11:58:40 +1030

    m

--- Utils.h
+++ Utils.h
@@ -5,17 +5,17 @@
 #include "GameEntity.h"
 #include "Effect.h"
 class Utils{
-    private:
-    std::tuple<int,int> randposition;
+    // private:
+    // std::tuple<int,int> randposition;
     public:
-    std::tuple<int,int> generateRandomPos(int gridWidth, int gridHeight){
+   static std::tuple<int,int> generateRandomPos(int gridWidth, int gridHeight){
         int x= std::rand() % gridWidth;
         int y=std::rand()% gridHeight;
-    randposition=std::make_tuple(x,y);
-    return randposition;
+   // randposition=std::make_tuple(x,y);
+    return std::tuple<int,int>(x,y);
     }
 
-    double calculateDistance(std::tuple<int, int>pos1, std::tuple<int,int>pos2){
+    static double calculateDistance(std::tuple<int, int>pos1, std::tuple<int,int>pos2){
      int x1 = std::get<0>(pos1);
     int y1 = std::get<1>(pos1);
     int x2 = std::get<0>(pos2);
@@ -24,7 +24,6 @@ class Utils{
     double dis_x = static_cast<double>(x2 - x1);
     double dis_y = static_cast<double>(y2 - y1);
 
-    // Euclidean distance formula: sqrt((x2 - x1)^2 + (y2 - y1)^2)
     double distance = std::sqrt(dis_x * dis_x + dis_y * dis_y);
 
     return distance;
